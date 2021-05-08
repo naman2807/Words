5264ff6fae777af33291d4b53022ee55947a35b7
naman2807 <naman.agarwal_cs19@gla.ac.in>
2021-05-08 09:54:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.wordsapp
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.ActivityMainBinding
+
+/**
+ * Main Activity and entry point for the app. Displays a RecyclerView of letters.
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private var isLinearLayoutManager = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        // Sets the LinearLayoutManager of the recyclerview
+
+        /**
+         * 1 way of setting recyclerView
+         */
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//        recyclerView.adapter = LetterAdapter(this)
+
+        /**
+         * 2 way of setting recyclerView
+         */
+        chooseLayout()
+    }
+
+    private fun chooseLayout(){
+        if(isLinearLayoutManager){
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        }else{
+            recyclerView.layoutManager = GridLayoutManager(this, 4)
+        }
+        recyclerView.adapter = LetterAdapter(this)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean("isLinearLayout", isLinearLayoutManager)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        isLinearLayoutManager = savedInstanceState.getBoolean("isLinearLayout")
+        chooseLayout()
+        super.onRestoreInstanceState(savedInstanceState)
+
+    }
+
+    private fun setIcon(menuItem : MenuItem?){
+        if(menuItem == null){
+            return
+        }
+        if(isLinearLayoutManager){
+            menuItem.icon = ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+        }else{
+            menuItem.icon = ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_menu, menu)
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_switch_layout -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+                chooseLayout()
+                setIcon(item)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
